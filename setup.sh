1ed6ca4eb4e5baf873968df02715f5ddb1b09cc6
Daniel Lu <daniel@lawrence.lu>
2019-08-30 14:00:57 -0700

    add dunstrc and user-dirs

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ ln -s $curdir/i3config ~/.config/i3/config
 ln -s $curdir/config.fish ~/.config/fish/config.fish
 ln -s $curdir/Xresources ~/.config/Xresources
 ln -s $curdir/fonts.conf ~/.config/fontconfig/fonts.conf
-
+ln -s $curdir/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -s $curdir/dunstrc ~/.config/dunst/dunstrc
