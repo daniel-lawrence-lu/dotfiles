b1ac75d372681e9d16f2b84230f35b8ed6e22541
Daniel Lu <daniel@lawrence.lu>
2020-01-18 16:49:23 -0800

    sway

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/fish ~/.config/fontconfig
 curdir=$(pwd)
 ln -s $curdir/nvimrc ~/.config/nvim/init.vim
 ln -s $curdir/i3config ~/.config/i3/config
+ln -s $curdir/i3config ~/.config/sway/config
 ln -s $curdir/config.fish ~/.config/fish/config.fish
 ln -s $curdir/Xresources ~/.config/Xresources
 ln -s $curdir/fonts.conf ~/.config/fontconfig/fonts.conf
